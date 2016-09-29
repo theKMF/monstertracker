e02ba19ae6d13e219b9c3e82ca61705b0cae697c
theKMF <tigerabbit@gmail.com>
2016-09-29 14:07:49 +0200

    New formula
    For the distance

--- src/script.js
+++ src/script.js
@@ -30,29 +30,27 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    distance( position.coords.latitude, position.coords.longitude, targetLat, targetLong, "K")
-   
+  var theDist =  getDistanceFromLatLonInKm( position.coords.latitude, position.coords.longitude, targetLat, targetLong);
+   demo.innerHTML =(theDist * 1000) + ' meters';
 }
 
-function showDist(la1,lo2,la3,lo4){
-    var theDist = distance
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
 
-
+  return d;
 }
 
-function distance(lat1, lon1, lat2, lon2, unit) {
-	var radlat1 = Math.PI * lat1/180
-	var radlat2 = Math.PI * lat2/180
-	var theta = lon1-lon2
-	var radtheta = Math.PI * theta/180
-	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-	dist = Math.acos(dist)
-	dist = dist * 180/Math.PI
-	dist = dist * 60 * 1.1515
-	if (unit=="K") { dist = dist * 1.609344 }
-	if (unit=="N") { dist = dist * 0.8684 }
-     demo.innerHTML =(dist * 1000) + ' meters';
-	return dist
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }
 
 function capture(item){
